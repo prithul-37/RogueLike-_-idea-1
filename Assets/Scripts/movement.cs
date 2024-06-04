3f4b066d8cd62389b8e57171b0c01725567afa32
prithul-37 <86515352+prithul-37@users.noreply.github.com>
2024-06-04 23:24:05 +0600

    Movement and Look at mouse

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    
+    private Vector2 mv;
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public Camera cam;
+    private Vector2 mousePos;
+    private Vector2 viewDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mv.x = Input.GetAxis("Horizontal");
+        mv.y = Input.GetAxis("Vertical");
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position+mv*moveSpeed*Time.deltaTime);
+        viewDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(viewDir.y, viewDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+
+    }
+}
