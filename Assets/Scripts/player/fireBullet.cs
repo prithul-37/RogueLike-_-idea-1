9ec9558edd11085273cbd0f8a6bc12f057eebe2f
prithul-37 <86515352+prithul-37@users.noreply.github.com>
2024-06-07 19:50:44 +0600

    Add IEnumarator

--- Assets/Scripts/player/fireBullet.cs
+++ Assets/Scripts/player/fireBullet.cs
@@ -6,25 +6,23 @@ public class fireBullet : MonoBehaviour
 {
     public GameObject bullet;
     public float bulletForce = 10f;
-    public int fireRate = 5;
-    private float timer = 0f;
+    public int fireRate = 5; //bullet per min
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(shoot());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator shoot()
     {
-        timer += Time.deltaTime;
-        if (timer> (float)1/fireRate)
+        WaitForSeconds wait = new WaitForSeconds((float)60/fireRate);
+        while (true) 
         {
-            GameObject GG =  Instantiate(bullet,transform.position,Quaternion.identity);
-            GG.GetComponent<Rigidbody2D>().AddForce(transform.up*bulletForce,ForceMode2D.Impulse);
-            timer = 0f;
-
+            yield return wait;
+            GameObject GG = Instantiate(bullet, transform.position, Quaternion.identity);
+            GG.GetComponent<Rigidbody2D>().AddForce(transform.up * bulletForce, ForceMode2D.Impulse);
         }
+        
     }
 }
